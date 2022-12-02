a84463f535e917b765d77d48ab8269bfac9562be
Christian Kellner <christian@kellner.me>
2022-12-02 19:52:44 +0100

    day1: implement second half

--- 1.swift
+++ 1.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 let i = try String(contentsOfFile: "1.txt")
-let x = i.split(separator: "\n\n").lazy.map{s in s.split(separator: "\n").map{x in Int(x)!}.reduce(0, +)}.max()!
-print(x)
+let top3 = i.split(separator: "\n\n").lazy.map{s in s.split(separator: "\n").map{x in Int(x)!}.reduce(0, +)}.sorted(by: >).prefix(3)
+print("top 3 \(top3)")
+let sum = top3.reduce(0, +)
+print("total: \(sum)")
 
 
