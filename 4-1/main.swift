eee18411b1e708978fe221b0acc12e2d31c645a8
Christian Kellner <christian@kellner.me>
2022-12-05 18:56:15 +0100

    day4: implement second half (same binary)

--- 4-1/main.swift
+++ 4-1/main.swift
@@ -1,6 +1,12 @@
 import Foundation
 import RegexBuilder
 
+enum Overlap {
+    case None
+    case Contains
+    case Contained
+    case Overlaps
+}
 
 struct Section {
 
@@ -15,6 +21,31 @@ struct Section {
     func contains(_ other: Section) -> Bool {
         return self.start <= other.start && self.end >= other.end
     } 
+
+    // very naive implementation
+    func overlaps(with other: Section) -> Overlap {
+        // self: (,); other: [,]
+        // 1: ( ... [ ... ] ... )
+        // 2: [ ... ( ... ) ... ]
+        //
+        // 3: ( ... [ ... ) ... ]
+        // 4: [ ... ( ... ] ... )
+        //
+        // 5: ( ... ) ... [ ... ]
+        // 6: [ ... ] ... ( ... )
+
+        if self.contains(other) {
+            return .Contains // 1
+        } else if other.contains(self) {
+            return .Contained // 2
+        } else if self.start <= other.start && self.end >= other.start {
+            return .Overlaps // 3
+        } else if other.start <= self.start && other.end >= self.start {
+            return .Overlaps // 4
+        } else {
+            return .None // 5, 6
+        }
+     }
 }
 
 func parseLine(_ str: String) -> (Section, Section) {
@@ -50,13 +81,22 @@ func parseLine(_ str: String) -> (Section, Section) {
     return (Section(a, b), Section(u, v))
 }
 
-var dups = 0
+var contained = 0
+var overlaps = 0
 
 while let line = readLine() {
     let (l, r) = parseLine(line)
-    if l.contains(r) || r.contains(l) {
-        dups += 1
+    let o = l.overlaps(with: r)
+
+    if o == .Overlaps {
+        overlaps += 1
+    }
+
+    if o == .Contains || o == .Contained {
+        contained += 1
     }
 }
 
-print(dups)
+print("Contained: \(contained)")
+print("Overlaps: \(overlaps + contained)")
+
